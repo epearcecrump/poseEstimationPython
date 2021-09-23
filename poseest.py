e28d4838f4e01ef7fb7d0b6392aa76bbd72f34f6
Edward Pearce-Crump <e.pearcecrump@googlemail.com>
2021-09-23 21:43:01 +0100

    Removed extra comments.

--- poseest.py
+++ poseest.py
@@ -5,6 +5,14 @@ import posesetup as setup           # Contains Body Parts dictionary and Pose Pa
                                     # (for drawing lines)
 
 def process(image, nnet, width, height):
+    # cv.dnn.blobFromImage() params:
+        # image: input image (with 1,3 or 4 channels) 
+        # scalefactor: a multipler for image values 
+            # (1.0 here but depends on nn that has been trained) 
+        # size: spatial size for output image 
+        # mean: scalar with mean values which are subtracted from channels 
+        # swapRB: True means swap BGR to RGB; False by default
+        # crop: indicates whether image will be cropped after resize or not
     nninput = cv.dnn.blobFromImage(image, 1.0, (width, height), 
                 (0, 0, 0), swapRB=False, crop=False) 
     nnet.setInput(nninput)
@@ -102,22 +110,8 @@ while cv.waitKey(100) < 0:
     imageH = image.shape[0] 
     imageW = image.shape[1]
     
-    # cv.dnn.blobFromImage() params:
-        # image: input image (with 1,3 or 4 channels) 
-        # scalefactor: a multipler for image values 
-            # (1.0 here but depends on nn that has been trained) 
-        # size: spatial size for output image 
-        # mean: scalar with mean values which are subtracted from channels 
-        # swapRB: True means swap BGR to RGB; False by default
-        # crop: indicates whether image will be cropped after resize or not
-    #nninput = cv.dnn.blobFromImage(image, 1.0, (args.width, args.height), 
-    #            (0, 0, 0), swapRB=False, crop=False) 
-    #nnet.setInput(nninput)
-
     # Obtain a 4-dimensional Mat object 
-        # an array of "heatmaps", the probability of a body part 
-        # being in location x, y
-    #nnoutput = nnet.forward()
+    # an array of "heatmaps", the probability of a body part being in location x, y
     nnoutput = process(image, nnet, args.width, args.height)
 
     outputW = nnoutput.shape[3]
@@ -125,56 +119,14 @@ while cv.waitKey(100) < 0:
     
     # Find the locations of the body parts for the input image
     locations = [] 
-    findBodyPartPositions(locations, nnoutput, imageW, imageH, outputW, outputH, args.threshold)
-
-    #for i in range(len(setup.BODY_PARTS)):
-        # Slice heatmap from output for the body part 
-        # first axis is 0 as we input only one image
-        #heatMap = nnoutput[0, i, :, :]
- 
-        # Finds the global minimum and maximum element values and their positions 
-            # (does not work with a multi-channel array) 
-            # (hence only a single pose can be detected this way)
-        #minVal, maxVal, minLoc, maxLoc = cv.minMaxLoc(heatMap)
-  
-        # Scale to image size
-        #W = (imageW * maxLoc[0]) / nnoutput.shape[3] 
-        #H = (imageH * maxLoc[1]) / nnoutput.shape[2]
-
-        # Add point pair if maxVal is higher than the given threshold
-        #if maxVal > args.threshold: 
-        #    locations.append((int(W), int(H))) 
-        #else:
-        #    locations.append(None)
+    findBodyPartPositions(locations, nnoutput, imageW, imageH, 
+            outputW, outputH, args.threshold)
    
     # Draw the skeleton lines for each of the POSE_PAIRS:
     drawSkeleton(locations, image)
 
-    #for pair in setup.POSE_PAIRS:
-        # Lookup the index in the BODY_PARTS dictionary 
-    #    idxFrom = setup.BODY_PARTS[pair[0]] 
-    #    idxTo = setup.BODY_PARTS[pair[1]]
-
-        # Only draw lines between locations that are in the locations array
-    #    if locations[idxFrom] and locations[idxTo]:
-            # cv.line() params: 
-                # frame, pt1, pt2, Scalar(colour:BGR), thick=1,linetype=8, shift=0)
-    #        cv.line(image, locations[idxFrom], locations[idxTo], (0, 255, 255), 4)
-
-            # cv.ellipse() params: 
-                # frame, center, axes, angle, startAngle, endAngle, 
-                # colour[, thickness[, linetype[, shift]]]
-    #        cv.ellipse(image, locations[idxFrom], (3,3), 0, 0, 360, 
-    #                (0, 0, 255), cv.FILLED) 
-    #        cv.ellipse(image, locations[idxTo], (3,3), 0, 0, 360, 
-    #                (0, 0, 255), cv.FILLED)
-
     # Frame information for image
     addFrameInfo(image, nnet)
-    #t, _ = nnet.getPerfProfile() 
-    #freq = cv.getTickFrequency() / 1000
-    #cv.putText(image, '%.2fms' % (t / freq), (10, 20), cv.FONT_HERSHEY_SIMPLEX,
-    #        0.5, (0, 0, 0)) 
 
     # Write image to output given on command line, and display image.
     cv.imwrite(args.output, image) 
